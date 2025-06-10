bbf78c9cffee1fc7988e1051de289ec14709b5a9
Mustafa <mustafaciceknote8@gmail.com>
2025-06-10 11:55:35 +0300

    flashlght first envsssss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   useLocalBackend: false,
   // Production ortamında doğrudan backend URL'sine istek atıyoruz
   // Vercel proxy'si ile sorun yaşandığı için doğrudan HTTPS URL kullanıyoruz
-  apiUrl: 'https://fanla-backen-zmnemj-981630-94-154-32-75.traefik.me/api'
+  apiUrl: 'http://fanla-backen-zmnemj-981630-94-154-32-75.traefik.me/api'
 };
